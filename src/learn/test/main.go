80d26f550d1bca615bbee68107a1d21d7ccf2c3b
kaka <1397393964@qq.com>
2020-07-23 23:55:25 +0800

    结构体

--- src/learn/test/main.go
+++ src/learn/test/main.go
@@ -1,4 +1,30 @@
 package main
 
+import "fmt"
+
+func testTowParam() {
+	a := 1000 % 5
+	b := 1001 % 5
+	c := 1002 % 5
+	d := 1003 % 5
+	e := 1004 % 5
+	f := 1005 % 5
+	g := 1006 % 5
+	h := 1007 % 5
+	i := 1008 % 5
+	j := 1009 % 5
+	fmt.Println(a)
+	fmt.Println(b)
+	fmt.Println(c)
+	fmt.Println(d)
+	fmt.Println(e)
+	fmt.Println(f)
+	fmt.Println(g)
+	fmt.Println(h)
+	fmt.Println(i)
+	fmt.Println(j)
+}
+
 func main() {
+	testTowParam()
 }
